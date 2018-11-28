0ca80395ad2ee2e71805533b56fe174f971058d8
Alex <alex@fourneaux.ca>
2018-11-28 18:03:10 -0500

    Initial Add

---
+++ Mana301_DataFilter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mana301_DataFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
